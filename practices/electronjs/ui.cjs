2cab7185902d6afff6ae11dc38b390176afb387e
kamawanu <kamawanu>
2023-09-08 14:24:53 +0900

    s/let/const

--- practices/electronjs/ui.cjs
+++ practices/electronjs/ui.cjs
@@ -1,24 +1,24 @@
 ///const electron = require("electron");
 ///const window.safeipc = window.safeipc;
-let button = document.querySelector("input#open");
+const button = document.querySelector("input#open");
 button.addEventListener("click", opensenddir);
 
 window.safeipc.on("add", fileadd);
 window.safeipc.on("clear", fileclear);
 
 function opensenddir(e) {
-    let odir = document.querySelector("input#directory").value;
+    const odir = document.querySelector("input#directory").value;
     console.log(odir);
     window.safeipc.send("dir", odir);
 }
 function fileadd(e, n) {
     //console.log(e);
     //console.log(n);
-    let _placement = document.querySelector("div#log");
+    const _placement = document.querySelector("div#log");
     _placement.innerHTML += n + "<BR>";
 }
 function fileclear(e) {
-    let _placement = document.querySelector("div#log");
+    const _placement = document.querySelector("div#log");
     _placement.innerHTML = "";
 
 }
