797b0f4d57fbaa83178470f4a706d315dead9f75
kamawanu <kamawanu>
2023-09-11 19:49:22 +0900

    .

--- concatmp4/concatmp4.sh
+++ concatmp4/concatmp4.sh
@@ -2,6 +2,10 @@
 for fn in $*
 do
     echo file "$fn" 
-done > files.txt
+done > /tmp/files.txt
 
-ffmpeg -f concat -safe 0 -i files.txt -c copy -fflags +genpts $( python3 $0.py $* ).mp4
+ffmpeg -f concat -safe 0 -i /tmp/files.txt -c copy -fflags +genpts $( python3 $0.py $* ).mp4 || exit 9
+for fn in $*
+do
+    mv "$fn" "$fn".BAK
+done
