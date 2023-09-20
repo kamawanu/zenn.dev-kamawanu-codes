b458775871c0eb4ad3724bf7ddd4c484e5149179
kamawanu <kamawanu>
2023-09-20 09:16:30 +0900

    https://zenn.dev/karamawanu/articles/175ddda196f108

---
+++ py27-fabric+docker/install.sh
@@ -0,0 +1,5 @@
+apk add python2 python2-dev py2-pip musl-dev linux-headers gcc libffi-dev
+pip2 install cffi==1.14
+apk add openssl-dev make
+pip2 install bcrypt==3.1.7 pynacl==1.3.0
+pip2 install fabric==1.14
