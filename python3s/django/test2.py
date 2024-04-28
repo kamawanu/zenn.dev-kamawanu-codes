cc9919b2a61c3bc8952f7e78f0ead8c9489363a9
kamawanu <kamawanu>
2024-04-28 23:36:32 +0900

    management_form

--- python3s/django/test2.py
+++ python3s/django/test2.py
@@ -3,17 +3,23 @@ from stubdjango.stubmodel import CommentForm,HelpTextContactForm
 from variantformset import variantformset
 
 inject = {
+    "form-MIN_NUM_FORMS": 2,
+    "form-MAX_NUM_FORMS": 2,
+    "form-INITIAL_FORMS": 2,
+    "form-TOTAL_FORMS": 2,
     "form-0-name": "*name",
     "form-1-subject": "*subject",
     "form-0-comment": "*comment",
     "form-1-message": "*message",
 }
 
-form0 = variantformset(inject,forms=[CommentForm,HelpTextContactForm])
+form0 = variantformset(inject,stubmanage=False,forms=[CommentForm,HelpTextContactForm])
 print(form0)
 ####breakpoint()
 form0.full_clean()
-print(form0)
+print(vars(form0.management_form))
+##print(form0.management_form)
+#print(form0)
 ###print(vars(form0.forms[0]))
 ###print(vars(form0.forms[1]))
 assert form0.is_valid() #, breakpoint()
--- python3s/django/variantformset.py
+++ python3s/django/variantformset.py
@@ -1,53 +1,65 @@
 import dataclasses
 from typing import Any, Type
-from django.forms.formsets import BaseFormSet,TOTAL_FORM_COUNT,INITIAL_FORM_COUNT
+from django.forms.formsets import BaseFormSet, TOTAL_FORM_COUNT, INITIAL_FORM_COUNT
 from django.forms.forms import Form
 
+
+@dataclasses.dataclass
+class stubmanagementform:
+    parent: "variantformset"
+
+    def is_valid(self):
+        return True
+
+    @property
+    def cleaned_data(self):
+        return {
+            # TOTAL_FORM_COUNT: len(self.parent.baseforms_),
+            # INITIAL_FORM_COUNT: len(self.parent.baseforms_),
+        }
+
+
 class variantformset(BaseFormSet):
-    baseforms: list[Type[Form]]
+    baseforms_: list[Type[Form]]
     form_renderer = None
     renderer = None
     selected: int = None
+    stubmanage_: bool
 
-    can_order=False
-    can_delete=False
-    max_num=None
-    validate_max=False
-    min_num=None
-    validate_min=False
-    ##absolute_max=None
-    can_delete_extra=True
-    renderer=None
+    can_order = False
+    can_delete = False
+    max_num = None
+    validate_max = False
+    min_num = None
+    validate_min = False
+    # absolute_max=None
+    can_delete_extra = True
+    renderer = None
 
     @property
     def absolute_max(self):
-        return len(self.baseforms)
-    
+        return len(self.baseforms_)
+
     @property
     def management_form(self) -> Any:
-        @dataclasses.dataclass
-        class stubmanagementform:
-            parent: "variantformset"
-            def is_valid(self):
-                return True
-            @property
-            def cleaned_data(self):
-                return {
-                    TOTAL_FORM_COUNT: len(self.parent.baseforms),
-                    INITIAL_FORM_COUNT: len(self.parent.baseforms),
-                }
-        return stubmanagementform(self)
+        return stubmanagementform(self) if self.stubmanage_ else super().management_form
 
-    def __init__(self, data: dict[str,Any], *, forms:list[Form], **kwargs ):
-        self.baseforms = forms
+    def total_form_count(self) -> int:
+        return len(self.baseforms_)
+
+    initial_form_count = total_form_count
+
+    def __init__(self, data: dict[str, Any], *, stubmanage=True, forms: list[Form], **kwargs):
+        self.baseforms_ = forms
+        self.stubmanage_ = stubmanage
         super().__init__(data, **kwargs)
 
-    def _construct_form(self,i,**kwargs):
+    def _construct_form(self, i, **kwargs):
         self.selected = i
-        form = super()._construct_form(i,**kwargs)
+        form = super()._construct_form(i, **kwargs)
         self.selected = None
         return form
 
     @property
     def form(self):
-        return self.baseforms[self.selected]
+        return self.baseforms_[self.selected]
