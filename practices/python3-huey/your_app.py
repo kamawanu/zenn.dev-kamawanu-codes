92b9e5f55571e012ce9e3e3c164b96671a9068a0
kamawanu <kamawanu>
2024-10-20 01:10:55 +0900

    some info

--- practices/python3-huey/your_app.py
+++ practices/python3-huey/your_app.py
@@ -1,14 +1,26 @@
 from huey import RedisHuey
 from huey import crontab
+#from huey import put
+#import pprint
+import os
 
 # Initialize Huey
 huey = RedisHuey('my_app', host='redis', port=6379)
 
+print(os.getpid())
+
 @huey.task()
 def add(a, b):
-    return a + b
+    print(os.getpid())
+    result = a + b
+    huey.put("result",result)
+
 
 if __name__ == "__main__":
     # Enqueue a task
     result = add(3, 4)
     print(f"Task enqueued, result: {result}")
+    print(f"Task enqueued, result: {vars(result)}")
+    result = add(3, 8)
+    print(f"Task enqueued, result: {result}")
+    print(f"Task enqueued, result: {vars(result)}")
