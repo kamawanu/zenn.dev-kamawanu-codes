4b51ad4bd1d73695ba11fcc4fc63c66424bbce2f
kamawanu <kamawanu>
2024-10-19 21:12:23 +0900

    tested

--- practices/python3-celery/app/celery_app.py
+++ practices/python3-celery/app/celery_app.py
@@ -1,9 +1,12 @@
 # celery.py
 from celery import Celery
 import os
+# worker-1  | [2024-10-19 12:09:39,724: ERROR/MainProcess] Received unregistered task of type 'tasks.add'.
+# worker-1  | The message has been ignored and discarded.
+import tasks
 
-broker_url = os.getenv('CELERY_BROKER_URL', 'redis://localhost:6379/0')
-result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
+broker_url = os.getenv('CELERY_BROKER_URL', 'redis://redis:6379/0')
+result_backend = os.getenv('CELERY_RESULT_BACKEND', 'redis://redis:6379/0')
 
 app = Celery('celery_demo',
              broker=broker_url,
